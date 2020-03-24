8b2b63a8e99470d6ba0e9242de3819da889519ac
Aaron Smith <aaroncosmith@gmail.com>
2020-03-24 15:15:15 -0400

    added delay loading, mimicing api

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -2,8 +2,17 @@ import React, { Component } from "react";
 
 class Quote extends Component {
     state = {
-        quote: "Chuck Norris has a RTX 4080ti!"
+        quote: "This app isn't as smart as Chuck Norris"
     }
+    
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                quote: "Chuck Norris has a RTX 4080ti!!"
+            })
+        }, 2000);
+    }
+
     render() {
         const { quote } = this.state;
         return (
